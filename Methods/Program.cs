25560f7b6cfff36c54341f8fdc2758352df47596
Andile Rodney <42125381+Andile-Rodney@users.noreply.github.com>
2021-02-19 06:31:09 +0200

    Add files via upload

---
+++ Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string result = GetUserDateOfBirth("Rodney Mthunzi");
+
+            Console.WriteLine(result);
+            //or a shorter way to write this
+
+            //Console.WriteLine(GetUserDateOfBirth("Rodney Mthunzi"));
+            
+
+
+        }
+
+        public static string GetUserDateOfBirth(string fullName)
+        {
+            return fullName + " was born on Tuesday";
+        }
+    }
+}
